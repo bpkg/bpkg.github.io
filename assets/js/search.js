2b6727b33bec1965d7fa9628a378cab3205662b0
Sam Likins <sam.likins@wsi-services.com>
2023-04-07 14:29:34 -0700

    Search feature (#74)
    * Updated configured production URL to drop unneeded ending slash.
    * Updated ATOM feed to remove extra slash from self link.
    * Updated reference to ~atom_path` and `rss_path`.
    * Updated sitemap to sort pages and posts alphabetically.
    * Added a JSON search feed file.
    * Added search page with JavaScript search.
    * Updated site template to add link for search page to header section.

---
+++ assets/js/search.js
@@ -0,0 +1,61 @@
+class PackageData {
+  title;
+  date;
+  author;
+  description;
+  category;
+  tags;
+  url;
+
+  constructor(data) {
+    this.title       = data.title;
+    this.date        = data.date;
+    this.author      = data.author;
+    this.description = data.description;
+    this.category    = data.category;
+    this.tags        = data.tags;
+    this.url         = data.url;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  const search = document.getElementById('search');
+  const results = document.getElementById('results');
+  let generated = '';
+  let data = [];
+  let search_term = '';
+
+  fetch('/feed/search.json')
+    .then((response) => response.json())
+    .then((data_server) => {
+      generated = data_server.generated;
+      data = data_server.posts.map(post => new PackageData(post));
+    });
+
+  search.addEventListener('input', (event) => {
+    search_term = event.target.value.toLowerCase();
+    showList();
+  });
+
+  const showList = () => {
+    results.innerHTML = '';
+
+    if (search_term.length == 0) return;
+
+    const match = new RegExp(`${search_term}`, 'gi');
+    let result = data.filter((name) => match.test(name.title));
+
+    if (result.length == 0) {
+      const li = document.createElement('li');
+      li.innerHTML = `Zero results found`;
+      results.appendChild(li);
+    }
+
+    result.forEach((package) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<a href="${package.url}">${package.title}</a>`;
+      results.appendChild(li);
+    });
+  };
+
+});
